c2e58a6fb39043478537718a95c93d565aae6ac9
sky lin <wengui.lin@live.com>
2016-06-01 13:19:09 +0800

    MsUni Initial

---
+++ MsUni/MsUni/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MsUni.MvcApplication" Language="C#" %>
---
+++ MsUni/MsUni/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MsUni.Startup))]
+namespace MsUni
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
